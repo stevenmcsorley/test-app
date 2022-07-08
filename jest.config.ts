62371f82e587dc143f9b6bb1c0ea7354231f3982
Steven McSorley <steven.mcsorley@eqtr.com>
2022-07-08 12:57:07 +0100

    better organize coverage configs out of packagejson into jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
 import nextJest from 'next/jest'
+
+const isDev = process.env.DEV === 'true'
 // Sync object
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -7,6 +9,30 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
+  collectCoverage: isDev ? false : true,
+  coverageReporters: ['json', 'text', 'html', 'lcov'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*/*.d.ts',
+    '!src/**/*/Loadable.{js,jsx,ts,tsx}',
+    '!src/**/*/types.ts',
+    '!src/**/store.ts',
+    '!src/index.tsx',
+    '!src/serviceWorker.ts',
+    '!<rootDir>/node_modules/',
+    '!**/templates/**',
+    '!**/template/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  coverageDirectory: 'coverage',
+  roots: ['<rootDir>/'],
   // Add more setup options before each test is run
   testMatch: ['**/*.test.tsx'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'], // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
